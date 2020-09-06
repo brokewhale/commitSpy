80901f54efb96ff8fa73978364b0948fada9e9eb
activenoob <yusufkehinde11@gmail.com>
2020-09-06 01:19:42 +0100

    home ui

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStateValue } from '../store/StateProvider';
 import { useEffect } from 'react';
+// import { SearchOutlined } from '@material-ui/icons';
+import ProjectCard from './ProjectCard';
 
 const Home = () => {
     // eslint-disable-next-line
@@ -22,7 +24,31 @@ const Home = () => {
     }, [token, dispatch])
 
     return (
-        <div>
+        <div className='home'>
+            <div className="home_grid">
+                <div className="info">
+                    <h1 className='name'>Hi User</h1>
+                    <p className='greetings'>Welcome back to workspace, we missed you</p>
+
+                    <div className="info__projects">
+                        <h3>Projects <span>(13)</span></h3>
+                        <div className="proj_grid">
+
+                            <ProjectCard />
+                            <ProjectCard />
+                            <ProjectCard />
+                            <ProjectCard />
+                            <ProjectCard />
+                            <ProjectCard />
+
+                        </div>
+
+                    </div>
+                </div>
+                <div className="showmain">
+
+                </div>
+            </div>
 
         </div>
     );
---
+++ src/Components/ProjectCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectCard = () => {
+    return (
+        <div className='projectcard'>
+            <div className='initials'>AB</div>
+            <p className='projname'>project name</p>
+        </div>
+    );
+};
+
+export default ProjectCard;
