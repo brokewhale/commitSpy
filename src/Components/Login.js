61384fbf1a43da969dadb7c3ecf4e7992149a703
activenoob <yusufkehinde11@gmail.com>
2020-10-11 21:36:04 +0100

    login change

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -57,77 +57,7 @@ const Login = () => {
 
 
     }
-    // const newlogin = () => {
-    //     console.log('click');
 
-
-
-    //     // After requesting Github access, Github redirects back to your app with a code parameter
-    //     const url = window.location.href;
-    //     const hasCode = url.includes("?code=");
-    //     const axios = require('axios');
-
-
-
-    //     // If Github API returns the code parameter
-    //     if (hasCode) {
-    //         const newUrl = url.split("?code=");
-    //         window.history.pushState({}, null, newUrl[0]);
-    //         setData({ ...data, isLoading: true });
-
-
-    //         // fetch user details with token
-    //         axios.post(`https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token`, {
-    //             client_id: state.client_id,
-    //             redirect_uri: state.redirect_uri,
-    //             client_secret: state.client_secret,
-    //             code: newUrl[1]
-    //         })
-    //             .then(response => {
-    //                 console.log(response.data)
-    //                 console.log(newUrl)
-
-    //                 let params = new URLSearchParams(response.data);
-
-    //                 setBtnload(true)
-    //                 axios.post('https://commitspy.herokuapp.com/api/users/regtoken',
-
-    //                     {
-
-    //                         // email: useremail,
-    //                         access_token: params.get("access_token"),
-    //                         // password: userpass,
-    //                         scope: params.get("scope"),
-    //                         token_type: params.get("token_type"),
-
-    //                     }
-    //                 )
-    //                     .then(function (response) {
-    //                         console.log(response);
-    //                         dispatch({
-    //                             type: "LOGIN",
-    //                             payload: { token: response.data.user.token, isLoggedIn: true }
-    //                         });
-    //                         setBtnload(false)
-
-    //                     })
-    //                     .catch(function (error) {
-    //                         console.log(error);
-    //                         setData({
-    //                             isLoading: false,
-    //                             errorMessage: "Sorry! Login failed"
-    //                         });
-    //                     });
-
-
-    //             })
-
-    //             .catch(error => {
-    //                 console.log(error);
-    //             });
-
-    //     }
-    // }
 
 
     if (state.isLoggedIn) {
@@ -161,12 +91,12 @@ const Login = () => {
                         forget password?
                     </Link >
 
-                    <button onClick={() => {
+                    <button className='gitlog' onClick={() => {
 
                         window.location.href = `https://github.com/login/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${redirect_uri}`;
                         setData({ ...data, errorMessage: "" });
                     }}>
-                        <GitHubIcon />
+                        Login With Github<GitHubIcon />
 
                     </button>
 
